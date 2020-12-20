4fec536fff2444d85a50a631996ae27695933da1
LaatuJ <jussi.markus.laatu@gmail.com>
2020-12-21 01:03:29 +0200

    wddwd

--- src/components/userInputs/Input.jsx
+++ src/components/userInputs/Input.jsx
@@ -1,5 +1,5 @@
 export const Input = (props) => {
-	const {type, onChange, value, label} = props;
+	const { onChange, value, label } = props;
 
 	const handleOnChange = (e) => {
 		onChange && onChange(e);
@@ -8,15 +8,12 @@ export const Input = (props) => {
 		<div>
 			<label>{label}</label>
 			<input
-				aria-label={type}
 				className={"input"}
-				type={type}
 				value={value}
 				onChange={(e) => {
 					handleOnChange(e.target.value)
 				}}
 			>
-
 			</input>
 		</div>
 	);
